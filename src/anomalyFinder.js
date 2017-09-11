d1674ef659f360d9dc1c801099b65367bc819235
sergibondarenko <sergibondarenko@gmail.com>
2017-09-11 17:24:16 +0200

    Create anomly finder class with example.

---
+++ src/anomalyFinder.js
@@ -0,0 +1,34 @@
+class AnomalyFinder {
+  
+  construct() {}
+
+  expectedValue(x, pow = 1) {
+    const accuracy = 0.1;
+    let sum = 0;
+
+    if (!x.length) return 0;
+
+    for(let i = 0; i < x.length; i++) {
+      sum += Math.pow(x[i], pow) / accuracy;
+    } 
+    return sum / (x.length / accuracy); 
+  }
+
+  standardDeviation(x, ex) {
+    const ex2 = this.expectedValue(x, 2);
+    ex = ex || this.expectedValue(x);
+    return Math.sqrt(ex2 - Math.pow(ex, 2)); 
+  }
+
+  find(x, value) {
+    const ex = this.expectedValue(x);
+    const stdv = this.standardDeviation(x, ex);
+    return !(Math.abs(ex - value) <= (3 * stdv));
+  }
+}
+
+const af = new AnomalyFinder();
+const x = [ 5, 15, 30 ];
+
+const isAnomaly = af.find(x, 55);
+console.log(`isAnomaly: ${isAnomaly}`);
